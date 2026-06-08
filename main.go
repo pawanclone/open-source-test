3cc2ae1c3c4b0dee320f62063786e30acb5d3e39
harshhsharmaa <harshhsharmaa57@gmail.com>
2026-06-08 10:20:39 +0530

    sum-func

--- main.go
+++ main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	sum(1,2,4,5,6)
 }
+
+func sum(nums ...int) int {
+	total := 0
+	for _, n := range nums {
+		total += n
+	}
+	return total
+}
+
